9d139c5a88bbdcdbaabc6e4c9fd89e9f6c2afe2a
JIAWEI68 <ang.jiawei56@gmail.com>
2021-05-10 23:35:46 +0800

    Music Stream V1

---
+++ app/src/main/java/com/example/musicstream/SongCollection.java
@@ -0,0 +1,13 @@
+package com.example.musicstream;
+
+public class SongCollection {
+    public SongCollection(){
+        Song TheWayYouLookTonight = new Song("S1001",
+                "The Way You Look Tonight",
+                "Micheal Buble",
+                "https://p.scdn.co/mp3-preview/a5b8972e764025020625bbf9c1c2bbb06e394a60?cid=2afe87a64b0042dabf51f37318616965",
+                4.66,
+                R.drawable.michael_buble_collection);
+    }
+
+}
